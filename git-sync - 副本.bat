1775cb77a89fc84217ada45d3ed91199ac87fb8d
sfefinance <sfefinance@163.com>
2026-01-06 14:57:04 +0800

    Auto commit: 2026/01/06 周二 14:57:04.50

--- git-sync - 副本.bat
+++ git-sync - 副本.bat
@@ -7,9 +7,11 @@ git commit -m "Auto commit: %date% %time%"
 
 echo Pulling latest changes...
 git pull myfork3 master
+git pull origin master
 
 echo Pushing changes...
-git push myfork3 master
+git push  myfork3 master
+git push origin master
 
 echo Git sync completed!
 pause
