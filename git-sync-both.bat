ffdcc75af1af4304e77edd8bcaacb2413362487e
sfefinance <sfefinance@163.com>
2026-04-28 21:35:56 +0800

    Auto commit: 周二 2026/04/28 21:35:56.32

---
+++ git-sync-both.bat
@@ -0,0 +1,23 @@
+@echo off
+cd /d D:\busd
+
+echo Adding all files...
+git add .
+
+echo Committing changes...
+git commit -m "Auto commit: %date% %time%"
+if %errorlevel% neq 0 (
+    echo No changes to commit, skipping push.
+    exit /b 0
+)
+
+echo Pulling latest changes (with proxy)...
+git -c http.proxy=http://127.0.0.1:7890 -c https.proxy=http://127.0.0.1:7890 pull myfork3 master
+git -c http.proxy=http://127.0.0.1:7890 -c https.proxy=http://127.0.0.1:7890 pull origin master
+
+echo Pushing changes (with proxy)...
+git -c http.proxy=http://127.0.0.1:7890 -c https.proxy=http://127.0.0.1:7890 push myfork3 master
+git -c http.proxy=http://127.0.0.1:7890 -c https.proxy=http://127.0.0.1:7890 push origin master
+
+echo Git sync completed!
+pause
