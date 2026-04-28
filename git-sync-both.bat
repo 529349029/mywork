cfeb433d03021ee6a9e889589801ac6783cf29c2
sfefinance <sfefinance@163.com>
2026-04-28 21:39:31 +0800

    Auto commit: 2026/04/28 周二 21:39:31.50

--- git-sync-both.bat
+++ git-sync-both.bat
@@ -11,12 +11,12 @@ if %errorlevel% neq 0 (
     exit /b 0
 )
 
-echo Pulling latest changes (with proxy)...
-git -c http.proxy=http://127.0.0.1:7890 -c https.proxy=http://127.0.0.1:7890 pull myfork3 master
-git -c http.proxy=http://127.0.0.1:7890 -c https.proxy=http://127.0.0.1:7890 pull origin master
+echo Pulling latest changes...
+git pull myfork3 master
+git pull origin master
 
-echo Pushing changes (with proxy)...
-git -c http.proxy=http://127.0.0.1:7890 -c https.proxy=http://127.0.0.1:7890 push myfork3 master
+echo Pushing changes...
+git push myfork3 master
 git -c http.proxy=http://127.0.0.1:7890 -c https.proxy=http://127.0.0.1:7890 push origin master
 
 echo Git sync completed!
