27ce761b8f6fc7f08668cdd4e4c0f40921e11557
sfefinance <sfefinance@163.com>
2026-04-28 21:31:23 +0800

    Auto commit: 2026/04/28 周二 21:31:23.82

--- git-sync-multi1.bat
+++ git-sync-multi1.bat
@@ -6,12 +6,12 @@ echo Committing changes...
 git commit -m "Auto commit: %date% %time%"
 
 echo Pulling latest changes...
-git pull myfork3 master
-git pull origin master
+git -c http.proxy=http://127.0.0.1:7890 -c https.proxy=http://127.0.0.1:7890 git pull myfork3 master
+git -c http.proxy=http://127.0.0.1:7890 -c https.proxy=http://127.0.0.1:7890 git pull origin master
 
 echo Pushing changes...
-git push  myfork3 master
-git push origin master
+git -c http.proxy=http://127.0.0.1:7890 -c https.proxy=http://127.0.0.1:7890 git push  myfork3 master
+git -c http.proxy=http://127.0.0.1:7890 -c https.proxy=http://127.0.0.1:7890 git push origin master
 
 echo Git sync completed!
 pause
